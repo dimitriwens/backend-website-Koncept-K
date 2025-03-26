e09e69cd5a92bea97cb62d738d645489814db292
Gustavo Espitia <146354509+gusespitia@users.noreply.github.com>
2025-03-26 21:48:59 +0100

    fixed

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,18 +1,17 @@
-
-
 module.exports = ({ env }) => ({
-upload: {
-    config:{
-    provider: '@strapi/provider-upload-cloudinary',
-    providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_API_KEY'),
-        api_secret: env('CLOUDINARY_API_SECRET'),
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_API_KEY'),
+          api_secret: env('CLOUDINARY_API_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
     },
-actionOptions: {
-    upload: {},
-    delete: {},
-    },
-},
-},
-});
+  });
+  
